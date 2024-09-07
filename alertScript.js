8f8b39573fad4e4b3444c37522a05af8001a6627
ekedal89 <ulli.ekedal@hotmail.com>
2024-09-07 10:33:36 +0200

    nye endringer

--- alertScript.js
+++ alertScript.js
@@ -2,3 +2,9 @@ function visBeskjed() {
     var navn = document.getElementById("name").value;
     alert("Hei på deg, " + navn + "!");
 }
+
+function visAlder() {
+    var alder = document.getElementById("age").value;
+    let x = 100-alder;
+    alert("Du er 100 år om " + x + " år!");
+}
