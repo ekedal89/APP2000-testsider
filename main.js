4c162b3b30e30841ae2cf2c1e8224606dd1811ef
ekedal89 <ulli.ekedal@hotmail.com>
2024-09-08 15:39:07 +0200

    ferdig

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function showMessage() {
     var message = "Hello, user!"; 
     alert(message);
 }
+
+function randomTall() {
+    var random = Math.floor(Math.random() * 100) + 1;
+    alert("Random tall: " + random);
+}
