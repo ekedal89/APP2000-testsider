594a187f0013b214158a0626de84289536af506e
ekedal89 <ulli.ekedal@hotmail.com>
2024-09-15 12:46:09 +0200

    2 nye funksjoner

--- test.js
+++ test.js
@@ -3,3 +3,12 @@ function changeColor() {
     document.body.style.backgroundColor = color;
     
 }
+function changeColor2() {
+    var color = document.getElementById("color2").value;
+    var headers = document.querySelectorAll("h2");
+    
+    headers.forEach(function(header) {
+        header.style.color = color;
+    });
+}
+
